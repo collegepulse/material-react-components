6d0d294a0c87160cfa944c2c81656c9057100193
Peter Mikitsh <peter.mikitsh@gmail.com>
2017-06-17 12:11:24 -0700

    [docs] [Navigation] refactor css

--- docs/DocNavigation.js
+++ docs/DocNavigation.js
@@ -1,34 +1,91 @@
 import {Link, NavLink} from 'react-router-dom';
 import List, {ListItem} from '../src/List';
 import {makeURL} from './globals';
+import Paper from '../src/Paper';
 import React from 'react';
 import Styles from './DocNavigation.css';
 import Typography from '../src/Typography';
 
+const items = [
+  {
+    label: 'AppBar',
+    url: makeURL('/AppBar')
+  },
+  {
+    label: 'BottomNavigation',
+    url: makeURL('/BottomNavigation')
+  },
+  {
+    label: 'Button',
+    url: makeURL('/Button')
+  },
+  {
+    label: 'Collapse',
+    url: makeURL('/Collapse')
+  },
+  {
+    label: 'Dialog',
+    url: makeURL('/Dialog')
+  },
+  {
+    label: 'List',
+    url: makeURL('/List')
+  },
+  {
+    label: 'Paper',
+    url: makeURL('/Paper')
+  },
+  {
+    label: 'SelectField',
+    url: makeURL('/SelectField')
+  },
+  {
+    label: 'SnackBar',
+    url: makeURL('/SnackBar')
+  },
+  {
+    label: 'Switch',
+    url: makeURL('/Switch')
+  },
+  {
+    label: 'Tabs',
+    url: makeURL('/Tabs')
+  },
+  {
+    label: 'TextField',
+    url: makeURL('/TextField')
+  },
+  {
+    label: 'Typography',
+    url: makeURL('/Typography')
+  }
+];
+
 function DocNavigation() {
   return (
-    <div style={{backgroundColor: '#fff', minHeight: '100vh', maxHeight: '100vh', width: '250px', overflow: 'hidden', zIndex: 1, borderRight: '1px solid #ddd'}}>
-      <div style={{overflowY: 'scroll', height: '100%'}}>
-        <div style={{padding: '15px'}}>
-          <Typography>
-            <Link to={makeURL()}>material-react-components</Link>
-            <div style={{marginTop: '5px'}}>By CollegePulse</div>
-          </Typography>
-        </div>
+    <div style={{flex: '0 0 auto', width: '200px'}}>
+      <Paper style={{borderRight: '1px solid #ddd', flex: '1 0 auto', height: '100vh', position: 'fixed', overflowY: 'scroll', zIndex: 1}}>
+        <Typography style={{padding: '15px'}}>
+          <Link to={makeURL()}>material-react-components</Link>
+          <div style={{marginTop: '5px'}}>By CollegePulse</div>
+        </Typography>
         <List>
-          <ListItem component={NavLink} activeClassName={Styles.active} className={Styles.inactive} primary={'AppBar'} to={makeURL('/AppBar')} />
-          <ListItem component={NavLink} activeClassName={Styles.active} className={Styles.inactive} primary={'BottomNavigation'} to={makeURL('/BottomNavigation')} />
-          <ListItem component={NavLink} activeClassName={Styles.active} className={Styles.inactive} primary={'Button'} to={makeURL('/Button')} />
-          <ListItem component={NavLink} activeClassName={Styles.active} className={Styles.inactive} primary={'Collapse'} to={makeURL('/Collapse')} />
-          <ListItem component={NavLink} activeClassName={Styles.active} className={Styles.inactive} primary={'Dialog'} to={makeURL('/Dialog')} />
-          <ListItem component={NavLink} activeClassName={Styles.active} className={Styles.inactive} primary={'List'} to={makeURL('/List')} />
-          <ListItem component={NavLink} activeClassName={Styles.active} className={Styles.inactive} primary={'Paper'} to={makeURL('/Paper')} />
-          <ListItem component={NavLink} activeClassName={Styles.active} className={Styles.inactive} primary={'Switch'} to={makeURL('/Switch')} />
-          <ListItem component={NavLink} activeClassName={Styles.active} className={Styles.inactive} primary={'Tabs'} to={makeURL('/Tabs')} />
-          <ListItem component={NavLink} activeClassName={Styles.active} className={Styles.inactive} primary={'TextField'} to={makeURL('/TextField')} />
-          <ListItem component={NavLink} activeClassName={Styles.active} className={Styles.inactive} primary={'Typography'} to={makeURL('/Typography')} />
+          {items.map(item => (
+            <ListItem
+              key={item.url}
+              component={NavLink}
+              activeClassName={Styles.active}
+              className={Styles.inactive}
+              primary={(
+                <div className={Styles.primary}>
+                  {item.label}
+                </div>
+              )}
+              to={item.url}
+            />
+          ))}
         </List>
-      </div>
+      </Paper>
     </div>
   );
 }
