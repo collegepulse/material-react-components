ad6a1e104de33d5e809a4537048a04876c3f6402
Peter Mikitsh <peter.mikitsh@gmail.com>
2017-06-19 11:38:21 -0700

    [DOMBodyRender] use reactive lifecycle hooks

--- src/utils/DOMBodyRender.js
+++ src/utils/DOMBodyRender.js
@@ -7,25 +7,40 @@ class DOMBodyRender extends React.Component {
   constructor(props) {
     super(props);
     this.node = null;
+    this.renderNode = this.renderNode.bind(this);
+    this.unrenderNode = this.unrenderNode.bind(this);
   }
 
-  componentWillMount() {
-    if (document && document.body) {
-      this.node = document.createElement('div');
-      document.body.appendChild(this.node);
-      render(this.props.children, this.node);
-    }
+  componentDidMount() {
+    this.renderNode();
   }
 
-  componentWillUpdate({children}) {
-    render(children, this.node);
+  componentDidUpdate() {
+    this.renderNode();
   }
 
   componentWillUnmount() {
+    this.unrenderNode();
+  }
+
+  unrenderNode() {
+    if (!this.node) {
+      return;
+    }
     unmountComponentAtNode(this.node);
     if (document && document.body) {
       document.body.removeChild(this.node);
     }
+  }
+
+  renderNode() {
+    if (!this.node) {
+      this.node = document.createElement('div');
+      if (document && document.body) {
+        document.body.appendChild(this.node);
+      }
+    }
+    render(this.props.children, this.node);
   }
 
   render() {
