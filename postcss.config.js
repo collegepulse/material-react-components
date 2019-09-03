23afe19f6e1dd1e93b5a7aa864374975eb56f669
Peter Mikitsh <peter.mikitsh@gmail.com>
2019-09-03 00:05:45 -0700

    chore: use .browserlistrc

--- postcss.config.js
+++ postcss.config.js
@@ -7,17 +7,6 @@ module.exports = () => ({
         cssvariables
       )
     }),
-    require('autoprefixer')({
-      browsers: [
-        'last 4 versions',
-        'Android >= 4.4',
-        'Chrome >= 49',
-        'Edge >= 12',
-        'Firefox >= 51',
-        'IE >= 11',
-        'iOS >= 8.4',
-        'Safari >= 8'
-      ]
-    })
+    require('autoprefixer')()
   ]
 });
